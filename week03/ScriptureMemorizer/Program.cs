0a6de5e1d6577819bd8f70e8b787ed1593ece097
Your Name <ngozichijike@gmail.com>
2025-09-23 11:40:21 +0100

    scripturememorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -7,7 +7,7 @@ class Program
         Reference reference = new Reference("John", 3, 16);
         Scripture scripture = new Scripture(reference, 
             "For God so loved the world that he gave his one and only Son, " +
-            "that whoever believes in him shall not perish but have eternal life..");
+            "that whoever believes in him shall not perish but have eternal life...");
 
         while (true)
         {
